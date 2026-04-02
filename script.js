07be935acc45aef68b3a201df868e41e1937d8ea
Navneet-Mishraa <navneetmishra2023@gmail.com>
2026-04-02 23:03:35 +0530

    comments

--- script.js
+++ script.js
@@ -16,13 +16,20 @@ const displayScore = function (sco) {
   document.querySelector('.score').textContent = sco;
 };
 
+// For Check Button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // No Number
   if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ No number!';
     displayMessage('⛔️ No number!');
+
+    // Input Number equal to SecretNumber
   } else if (guess === secretNumber) {
     displayMessage('🎉 Correct Number!');
+
+    // document.querySelector('.number').textContent = secretNumber;
     displayNumber(secretNumber);
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -31,10 +38,14 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
+
+    // Input Number high/less than secretNumber
   } else if (guess !== secretNumber) {
     if (score > 1) {
       displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low');
       score--;
+
+      // document.querySelector('.score').textContent = score;
       displayScore(score);
     } else {
       displayMessage('💥 You loose the game!');
@@ -43,6 +54,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
+// For Again Button
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
